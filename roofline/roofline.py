2116fd141861ccb9fe346fbf0cfb6fb44d2d8a32
ocnkr <ocankur@umd.edu>
2023-12-05 12:53:19 -0500

    finalize ncu roofline calculation

--- roofline/roofline.py
+++ roofline/roofline.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Parallel Software and Systems Group, University of 
+# Copyright (c) 2023, Parallel Software and Systems Group, University of
 # Maryland. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
@@ -44,90 +44,359 @@ class Roofline:
                 )
 
     def analyze_ncu(self, data):
+        """
+        This function calculates all the required points to create a roofline plot.
+        Specifically, it calculates double, single, half precisions and tensor core.
+        It also calculates DRAM, L1, and L2 ceilings and achieved values.
+        """
         dataframe = data.copy(deep=True)
-
         dataframe = dataframe[self.data["Kernel Name"].notna()]
 
-        max_single_sm_per_cycle = dataframe[
-            "derived__sm__sass_thread_inst_executed_op_ffma_pred_on_x2"
-        ].astype(float)
+        def _calculate_l1(peakwork, achieved_performance, precision):
+            # L1 AI
+            L1_roof_AI = "{}_l1_roof_AI".format(precision)
+            L1_achieved_AI = "{}_l1_achieved_AI".format(precision)
+
+            dataframe[L1_roof_AI] = peakwork / l1_peaktraffic
+            dataframe[L1_achieved_AI] = achieved_performance / l1_cycles_per_sec_sum
+
+            columns.extend([L1_roof_AI, L1_achieved_AI])
+
+        def _calculate_l2(peakwork, achieved_performance, precision):
+            # L2 AI
+            L2_roof_AI = "{}_l2_roof_AI".format(precision)
+            L2_achieved_AI = "{}_l2_achieved_AI".format(precision)
+
+            dataframe[L2_roof_AI] = peakwork / l2_peaktraffic
+            dataframe[L2_achieved_AI.format(precision)] = (
+                achieved_performance / l2_cycles_per_sec_sum
+            )
+
+            columns.extend([L2_roof_AI, L2_achieved_AI])
+
+        def _calculate_single():
+            max_sm_per_cycle = (
+                dataframe[
+                    "sm__sass_thread_inst_executed_op_ffma_pred_on.sum.peak_sustained"
+                ].astype(float)
+                * 2
+            )
+
+            peakwork = max_sm_per_cycle * cycles_sm_per_sec
+
+            # DRAM roof AI
+            dataframe["single_dram_roof_AI"] = peakwork / dram_peaktraffic
+
+            # DRAM Achieved
+            fadd_per_cycle = dataframe[
+                "smsp__sass_thread_inst_executed_op_fadd_pred_on.sum.per_cycle_elapsed"
+            ].astype(float)
+            fmul_per_cycle = dataframe[
+                "smsp__sass_thread_inst_executed_op_fmul_pred_on.sum.per_cycle_elapsed"
+            ].astype(float)
+            ffma_per_cycle = (
+                dataframe[
+                    "smsp__sass_thread_inst_executed_op_ffma_pred_on.sum.per_cycle_elapsed"
+                ].astype(float)
+                * 2
+            )
+            cycles_smsp_per_sec = dataframe[
+                "smsp__cycles_elapsed.avg.per_second"
+            ].astype(float)
+
+            # DRAM, L1, and L2 Achieved Work
+            achieved_performance = (
+                fadd_per_cycle + fmul_per_cycle + ffma_per_cycle
+            ) * cycles_smsp_per_sec
+
+            dataframe["single_dram_achieved_AI"] = (
+                achieved_performance / dram_cycles_per_sec_sum
+            )
+
+            dataframe["single_achieved_performance"] = achieved_performance
+            dataframe["single_peak_performance"] = peakwork
+
+            columns.extend(
+                [
+                    "single_dram_roof_AI",
+                    "single_dram_achieved_AI",
+                    "single_achieved_performance",
+                    "single_peak_performance",
+                ]
+            )
+
+            if l1:
+                _calculate_l1(peakwork, achieved_performance, precision="single")
+            if l2:
+                _calculate_l2(peakwork, achieved_performance, precision="single")
+
+        def _calculate_double():
+            max_sm_per_cycle = (
+                dataframe[
+                    "sm__sass_thread_inst_executed_op_dfma_pred_on.sum.peak_sustained"
+                ].astype(float)
+                * 2
+            )
+
+            peakwork = max_sm_per_cycle * cycles_sm_per_sec
+
+            # DRAM roof AI
+            dataframe["double_dram_roof_AI"] = peakwork / dram_peaktraffic
+
+            # DRAM Achieved
+            dadd_per_cycle = dataframe[
+                "smsp__sass_thread_inst_executed_op_dadd_pred_on.sum.per_cycle_elapsed"
+            ].astype(float)
+            dmul_per_cycle = dataframe[
+                "smsp__sass_thread_inst_executed_op_dmul_pred_on.sum.per_cycle_elapsed"
+            ].astype(float)
+            dfma_per_cycle = (
+                dataframe[
+                    "smsp__sass_thread_inst_executed_op_dfma_pred_on.sum.per_cycle_elapsed"
+                ].astype(float)
+                * 2
+            )
+            cycles_smsp_per_sec = dataframe[
+                "smsp__cycles_elapsed.avg.per_second"
+            ].astype(float)
+
+            # DRAM, L1, and L2 Achieved Work
+            achieved_performance = (
+                dadd_per_cycle + dmul_per_cycle + dfma_per_cycle
+            ) * cycles_smsp_per_sec
+
+            dataframe["double_dram_achieved_AI"] = (
+                achieved_performance / dram_cycles_per_sec_sum
+            )
+
+            dataframe["double_achieved_performance"] = achieved_performance
+            dataframe["double_peak_performance"] = peakwork
+
+            columns.extend(
+                [
+                    "double_dram_roof_AI",
+                    "double_dram_achieved_AI",
+                    "double_achieved_performance",
+                    "double_peak_performance",
+                ]
+            )
+
+            if l1:
+                _calculate_l1(peakwork, achieved_performance, precision="double")
+            if l2:
+                _calculate_l2(peakwork, achieved_performance, precision="double")
+
+        def _calculate_half():
+            max_sm_per_cycle = (
+                dataframe[
+                    "sm__sass_thread_inst_executed_op_hfma_pred_on.sum.peak_sustained"
+                ].astype(float)
+                * 2
+            )
+
+            peakwork = max_sm_per_cycle * cycles_sm_per_sec
+
+            # DRAM roof AI
+            dataframe["half_dram_roof_AI"] = peakwork / dram_peaktraffic
+
+            # DRAM Achieved
+            hadd_per_cycle = dataframe[
+                "smsp__sass_thread_inst_executed_op_hadd_pred_on.sum.per_cycle_elapsed"
+            ].astype(float)
+            hmul_per_cycle = dataframe[
+                "smsp__sass_thread_inst_executed_op_hmul_pred_on.sum.per_cycle_elapsed"
+            ].astype(float)
+            hfma_per_cycle = (
+                dataframe[
+                    "smsp__sass_thread_inst_executed_op_hfma_pred_on.sum.per_cycle_elapsed"
+                ].astype(float)
+                * 2
+            )
+            cycles_smsp_per_sec = dataframe[
+                "smsp__cycles_elapsed.avg.per_second"
+            ].astype(float)
+
+            # DRAM, L1, and L2 Achieved Work
+            achieved_performance = (
+                hadd_per_cycle + hmul_per_cycle + hfma_per_cycle
+            ) * cycles_smsp_per_sec
+
+            dataframe["half_dram_achieved_AI"] = (
+                achieved_performance / dram_cycles_per_sec_sum
+            )
+
+            dataframe["half_achieved_performance"] = achieved_performance
+            dataframe["half_peak_performance"] = peakwork
+
+            columns.extend(
+                [
+                    "half_dram_roof_AI",
+                    "half_dram_achieved_AI",
+                    "half_achieved_performance",
+                    "half_peak_performance",
+                ]
+            )
+
+            if l1:
+                _calculate_l1(peakwork, achieved_performance, precision="half")
+            if l2:
+                _calculate_l2(peakwork, achieved_performance, precision="half")
+
+        def _calculate_tensor():
+            max_sm_per_cycle = (
+                dataframe["sm__inst_executed_pipe_tensor.sum.peak_sustained"].astype(
+                    float
+                )
+                * 512
+            )
+
+            peakwork = max_sm_per_cycle * cycles_sm_per_sec
+
+            # DRAM roof AI
+            dataframe["tensor_dram_roof_AI"] = peakwork / dram_peaktraffic
+
+            # DRAM Achieved
+            tensor_per_cycle = (
+                dataframe[
+                    "smsp__inst_executed_pipe_tensor.sum.per_cycle_elapsed"
+                ].astype(float)
+                * 512
+            )
+            cycles_smsp_per_sec = dataframe[
+                "smsp__cycles_elapsed.avg.per_second"
+            ].astype(float)
+
+            # DRAM, L1, and L2 Achieved Work
+            achieved_performance = tensor_per_cycle * cycles_smsp_per_sec
+
+            dataframe["tensor_dram_achieved_AI"] = (
+                achieved_performance / dram_cycles_per_sec_sum
+            )
+
+            dataframe["tensor_achieved_performance"] = achieved_performance
+            dataframe["tensor_peak_performance"] = peakwork
+
+            columns.extend(
+                [
+                    "tensor_dram_roof_AI",
+                    "tensor_dram_achieved_AI",
+                    "tensor_achieved_performance",
+                    "tensor_peak_performance",
+                ]
+            )
+
+            if l1:
+                _calculate_l1(peakwork, achieved_performance, precision="tensor")
+            if l2:
+                _calculate_l2(peakwork, achieved_performance, precision="tensor")
+
+        l1 = False
+        l2 = False
+        if "l1tex__t_bytes.sum.peak_sustained" in dataframe.columns:
+            l1 = True
+        if "lts__t_bytes.sum.peak_sustained" in dataframe.columns:
+            l2 = True
+
         cycles_sm_per_sec = dataframe["sm__cycles_elapsed.avg.per_second"].astype(float)
-        bytes_dram_per_cycle = dataframe["dram__bytes.sum.peak_sustained"].astype(float)
-        cycles_dram_per_sec = dataframe["dram__cycles_elapsed.avg.per_second"].astype(
+
+        # DRAM Roofline
+        dram_bytes_per_cycle = dataframe["dram__bytes.sum.peak_sustained"].astype(float)
+        dram_cycles_per_sec = dataframe["dram__cycles_elapsed.avg.per_second"].astype(
             float
         )
 
-        single_peakwork = max_single_sm_per_cycle * cycles_sm_per_sec
-        single_peaktraffic = bytes_dram_per_cycle * cycles_dram_per_sec
+        dram_peaktraffic = dram_bytes_per_cycle * dram_cycles_per_sec
 
-        max_double_sm_per_cycle = dataframe[
-            "derived__sm__sass_thread_inst_executed_op_pmc_perf_dataframema_pred_on_x2"
-        ].astype(float)
+        # DRAM Achieved Traffic
+        dram_cycles_per_sec_sum = dataframe["dram__bytes.sum.per_second"].astype(float)
 
-        double_peakwork = max_double_sm_per_cycle * cycles_sm_per_sec
-        double_peaktraffic = bytes_dram_per_cycle * cycles_dram_per_sec
+        # L1 Roofline
+        if l1:
+            l1_bytes_per_cycle = dataframe["l1tex__t_bytes.sum.peak_sustained"].astype(
+                float
+            )
+            l1_cycles_per_sec = dataframe[
+                "l1tex__cycles_elapsed.avg.per_second"
+            ].astype(float)
 
-        single_fadd_per_cycle = dataframe[
-            "smsp__sass_thread_inst_executed_op_fadd_pred_on.sum.per_cycle_elapsed"
-        ].astype(float)
-        single_fmul_per_cycle = dataframe[
-            "smsp__sass_thread_inst_executed_op_fmul_pred_on.sum.per_cycle_elapsed"
-        ].astype(float)
-        single_ffma_per_cycle = dataframe[
-            "derived__smsp__sass_thread_inst_executed_op_ffma_pred_on_x2"
-        ].astype(float)
-        cycles_smsp_per_sec = dataframe["smsp__cycles_elapsed.avg.per_second"].astype(
-            float
-        )
-        cycles_dram_per_sec_sum = dataframe["dram__bytes.sum.per_second"].astype(float)
+            l1_peaktraffic = l1_bytes_per_cycle * l1_cycles_per_sec
 
-        dataframe["single_performance"] = (
-            single_fadd_per_cycle + single_fmul_per_cycle + single_ffma_per_cycle
-        ) * cycles_smsp_per_sec
+            # L1 Achieved Traffic
+            l1_cycles_per_sec_sum = dataframe["l1tex__t_bytes.sum.per_second"].astype(
+                float
+            )
 
-        double_fadd_per_cycle = dataframe[
-            "smsp__sass_thread_inst_executed_op_dadd_pred_on.sum.per_cycle_elapsed"
-        ].astype(float)
-        double_fmul_per_cycle = dataframe[
-            "smsp__sass_thread_inst_executed_op_dmul_pred_on.sum.per_cycle_elapsed"
-        ].astype(float)
-        double_ffma_per_cycle = dataframe[
-            "derived__smsp__sass_thread_inst_executed_op_pmc_perf_dataframema_pred_on_x2"
-        ].astype(float)
-        cycles_smsp_per_sec = dataframe["smsp__cycles_elapsed.avg.per_second"].astype(
-            float
-        )
-        cycles_dram_per_sec_sum = dataframe["dram__bytes.sum.per_second"].astype(float)
+        # L2 Roofline
+        if l2:
+            l2_bytes_per_cycle = dataframe["lts__t_bytes.sum.peak_sustained"].astype(
+                float
+            )
+            l2_cycles_per_sec = dataframe["lts__cycles_elapsed.avg.per_second"].astype(
+                float
+            )
 
-        double_performance = (
-            double_fadd_per_cycle + double_fmul_per_cycle + double_ffma_per_cycle
-        ) * cycles_smsp_per_sec
+            l2_peaktraffic = l2_bytes_per_cycle * l2_cycles_per_sec
 
-        dataframe["achieved_aritmetic_intensity"] = (
-            double_performance / cycles_dram_per_sec_sum
-        ) / 1000
-        dataframe["aritmetic_intensity_double"] = (
-            double_peakwork / double_peaktraffic
-        ) / 1000
-        dataframe["aritmetic_intensity_single"] = (
-            single_peakwork / single_peaktraffic
-        ) / 1000
+            # L2 Achieved Traffic
+            l2_cycles_per_sec_sum = dataframe["lts__t_bytes.sum.per_second"].astype(
+                float
+            )
 
-        dataframe["achieved_performance"] = double_performance / 1000
-        dataframe["peak_performance_double"] = double_peakwork / 1000
-        dataframe["peak_performance_single"] = single_peakwork / 1000
+        columns = ["Kernel Name"]
+        _calculate_single()
 
-        return dataframe[
-            [
-                "Kernel Name",
-                "achieved_aritmetic_intensity",
-                "aritmetic_intensity_double",
-                "aritmetic_intensity_single",
-                "achieved_performance",
-                "peak_performance_double",
-                "peak_performance_single",
-            ]
-        ]
+        _calculate_double()
+
+        if (
+            "sm__sass_thread_inst_executed_op_hfma_pred_on.sum.peak_sustained"
+            in dataframe.columns
+        ):
+            _calculate_half()
+
+        if "sm__inst_executed_pipe_tensor.sum.peak_sustained" in dataframe.columns:
+            _calculate_tensor()
+
+        dataframe = dataframe[columns]
+
+        return dataframe
+
+    # TODO: Draft
+    def plot_ncu(
+        self,
+        dataframe,
+        precisions=["single", "double"],
+        types=["dram"],
+        kernels=None,
+        summary=False,
+    ):
+        """
+        precision: list of precisions: single, double, half, tensor
+        list of hardware options: dram, l1, l2
+        """
+        start_AI = 0.001
+
+        if summary:
+            dataframe = dataframe.groupby("Kernel Name", as_index=False).agg(np.mean)
+
+        columns = list(dataframe.filter(regex="single_dram|double_dram").columns)
+        points = []
+        max_values = {}
+        for precision in precisions:
+            achieved_performance = "{}_achieved_performance".format(precision)
+            peak_performance = "{}_peak_performance".format(precision)
+            max_values[peak_performance] = dataframe[peak_performance].max()
+            for type in types:
+                achieved_type = "{}_{}_achieved_AI".format(precision, type)
+                roof_type = "{}_{}_roof_AI".format(precision, type)
+                starting_point = "{}_{}_start_performance".format(precision, type)
+                dataframe[starting_point] = (
+                    dataframe[peak_performance] / dataframe[roof_type]
+                ) * start_AI
+                points.append([achieved_performance, achieved_type])
+                points.append([peak_performance, roof_type])
+
+        for point in points:
+            print(dataframe[point].iloc[2])
 
     def analyze_omniperf(self, data):
         def calculate_application_performance(pmc_perf_dataframe):

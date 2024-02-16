04c2a545a5d471298db35737257caee3acbe7342
Abhinav Bhatele <bhatele@cs.umd.edu>
2024-02-16 00:19:46 -0500

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup
+
+setup(
+    name="py-roofline",
+    version="0.0.1",
+    description="A Python library for creating rooflines",
+    url="https://github.com/hpcgroup/py-roofline",
+    author="Abhinav Bhatele",
+    author_email="bhatele@cs.umd.edu",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="distributed computing, parallel computing, GPU traces",
+)
